6bef2ad5cf9ef3f922644fb10619928d0b93ecd5
Andrea Grandi <a.grandi@gmail.com>
2014-03-13 15:53:58 +0000

    Fix the tickets cursor and print each row found.

--- tractogithub.py
+++ tractogithub.py
@@ -7,5 +7,12 @@ parser.add_argument('--dbuser', help='Database user')
 parser.add_argument('--dbpassword', help='Database password')
 args = parser.parse_args()
 
-connection = psycopg2.connect(dbname=args.dbname, user=args.user, password=args.password)
-cur = connection.cursor()
+connection = psycopg2.connect(dbname=args.dbname, user=args.dbuser, password=args.dbpassword)
+cursor = connection.cursor()
+cursor.execute("SELECT id, summary, description , owner, milestone, component, status FROM ticket ORDER BY id;")
+
+for row in cursor:
+	print row
+
+cursor.close()
+connection.close()
