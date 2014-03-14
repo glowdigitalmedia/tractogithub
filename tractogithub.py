09f9dcb69a6bc92ffdf06a6609e234ecd8831238
Andrea Grandi <a.grandi@gmail.com>
2014-03-14 10:45:30 +0000

    Load the users map from a json file so the Issues can be assigned to the proper GitHub users.

--- tractogithub.py
+++ tractogithub.py
@@ -1,5 +1,6 @@
 import psycopg2
 import argparse
+import json
 from pygithub3 import Github
 
 parser = argparse.ArgumentParser(description='Trac tickets to GitHub Issues migration tool.')
@@ -9,8 +10,16 @@ parser.add_argument('--dbpassword', help='Database password')
 parser.add_argument('--ghtoken', help='GitHub token')
 parser.add_argument('--ghuser', help='GitHub user')
 parser.add_argument('--ghrepo', help='GitHub repository')
+parser.add_argument('--users', help='Users map JSON file (users.json)')
 args = parser.parse_args()
 
+# Load users map from a .json file
+# the file must contain a key->value map with trac_user->github_user
+# so the Issues can be assigned properly
+users_json_data = open(args.users).read()
+users_map = json.loads(users_json_data)
+
+# Initialize the GitHub API with access token, user and repository
 gh = Github(token=args.ghtoken, user=args.ghuser, repo=args.ghrepo)
 
 connection = psycopg2.connect(dbname=args.dbname, user=args.dbuser, password=args.dbpassword)
